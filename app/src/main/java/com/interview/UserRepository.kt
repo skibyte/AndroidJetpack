2d5b33224e789dc96d81b94eb2a2e03af4a637f6
Fernando Castillo <skibyte@gmail.com>
2020-10-01 06:05:35 -0500

    Simulate network operation

--- app/src/main/java/com/interview/UserRepository.kt
+++ app/src/main/java/com/interview/UserRepository.kt
@@ -2,12 +2,21 @@ package com.interview
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
 
 class UserRepository {
     private val persons = listOf(Person("Andres", 20), Person("Hector", 27), Person("Sara", 35))
     private val mutableLiveData  by lazy {
         val liveData = MutableLiveData<List<Person>>()
-        liveData.value = persons
+        GlobalScope.launch (Dispatchers.IO){
+            delay(600)
+            liveData.postValue(persons)
+        }
+
         liveData
     }
 
